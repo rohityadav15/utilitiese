b32b71abd90acece7bdf4f123c7faf6bfe6bd6ce
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2024-12-30 17:43:48 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -3,34 +3,77 @@ import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Todo from "./pages/Todo";
 import Factorial from "./pages/Factorial";
+import AgeCalculator from "./pages/AgeCalculator";
+import UnitConversion from "./pages/UnitConversion";
+import NumberToWord from "./pages/NumberToWord";
 
 function App() {
   return (
     <Router>
-      <Box>
-        <nav>
-          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-            Home
-          </Link>
-          <Link to="/toDo" style={{ color: "white", textDecoration: "none" }}>
-            Todo
-          </Link>
-          <Link
-            to="/factorial"
-            style={{ color: "white", textDecoration: "none" }}
+        <Box>
+          <nav
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              // width: "90%",
+              alignItems: "center",
+              // justifyContent:"center",
+              backgroundColor: "black",
+              padding: 8,
+            }}
           >
-            Factorial
-          </Link>
-        </nav>
-      </Box>
-      <Box>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/toDo" element={<Todo />} />
-          <Route path="/factorial" element={<Factorial />} />
-        </Routes>
-      </Box>
-    </Router>
+            <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+              Home
+            </Link>
+            <Link to="/ageCalculator" style={{ color: "white", textDecoration: "none" }}>
+              Age Calculator
+            </Link>
+            <Link to="/unitConversion" style={{ color: "white", textDecoration: "none" }}>
+              Unit Conversion
+            </Link>
+            <Link
+              to="/toDo"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              Todo
+            </Link>
+            <Link
+              to="/factorial"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              Factorial
+            </Link>
+            <Link
+              to="/numberToWord"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              Number To word
+            </Link>
+          </nav>
+        </Box>
+        <Box
+          style={{
+            display: "flex",
+            alignItems:"center",
+            justifyContent:"center",
+            width: "215vh",
+            height:"90vh",
+            margin:"1%",
+            borderRadius:"1%",
+            boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/factorial" element={<Factorial />} />
+            <Route path="/toDo" element={<Todo />} />
+            <Route path="/ageCalculator" element={<AgeCalculator />} />
+            <Route path="/unitConversion" element={<UnitConversion />} />
+            <Route path="/numberToWord" element={<NumberToWord />} />
+          </Routes>
+        </Box>
+      </Router>
   );
 }
 
