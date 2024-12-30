9b1310e7db62a1b3f6f6be0620134744881b295c
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2024-12-30 20:24:22 +0530

    text field

--- src/pages/Factorial.js
+++ src/pages/Factorial.js
@@ -1,7 +1,25 @@
-const Factorial=()=>{
-    return(
-        <>
-        Factorial Number</>
-    )
-}
+import { Box, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+const Factorial = () => {
+    const [value, setValue]= useState(0);
+
+    const handleValue=(e)=>{
+        setValue(e.target.value)
+    }
+    
+
+  return (
+    <Box>
+      <Box style={{display:"flex",flexDirection:"row"}}>
+        <Typography m={1}>Enter the value: </Typography>
+        <TextField
+          label="Enter value"
+          onChange={handleValue}
+          size="small"
+        />
+      </Box> 
+    </Box>
+  );
+};
 export default Factorial;
