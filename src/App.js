cc526cf4ae71a05824de45c12bd934c8fb321573
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2024-12-30 15:24:29 +0530

    json file add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import { Box } from "@mui/material";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Todo from "./pages/Todo";
+import Factorial from "./pages/Factorial";
 
 function App() {
   return (
-    <Box>
-
-    </Box>
+    <Router>
+      <Box>
+        <nav>
+          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+            Home
+          </Link>
+          <Link to="/toDo" style={{ color: "white", textDecoration: "none" }}>
+            Todo
+          </Link>
+          <Link
+            to="/factorial"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            Factorial
+          </Link>
+        </nav>
+      </Box>
+      <Box>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/toDo" element={<Todo />} />
+          <Route path="/factorial" element={<Factorial />} />
+        </Routes>
+      </Box>
+    </Router>
   );
 }
 
