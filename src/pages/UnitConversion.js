80d2c9e4f3ec486ed5483a72373e1f89351b9795
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2024-12-31 23:13:19 +0530

    DropDown create

--- src/pages/UnitConversion.js
+++ src/pages/UnitConversion.js
@@ -1,10 +1,38 @@
-import { Box } from "@mui/material";
+import { Box, MenuItem, Select } from "@mui/material";
 
-const UnitConversion=()=>{
-    return(
+const UnitConversion = () => {
+  return (
+    <Box>
+      Conversion
+      <Box>
         <Box>
-        Conversion
+          <Select
+            labelId="unit-select-label"
+            id="unit-select"
+            //   value={selectedUnit}
+            //   onChange={handleChange}
+            label="Select Unit"
+          >
+            <MenuItem value="cm">cm</MenuItem>
+            <MenuItem value="meter">meter</MenuItem>
+            <MenuItem value="inch">inch</MenuItem>
+          </Select>
         </Box>
-    )
-}
+        <box>
+          <Select
+            labelId="unit-select-label"
+            id="unit-select"
+            //   value={selectedUnit}
+            //   onChange={handleChange}
+            label="Select Unit"
+          >
+            <MenuItem value="cm">cm</MenuItem>
+            <MenuItem value="meter">meter</MenuItem>
+            <MenuItem value="inch">inch</MenuItem>
+          </Select>
+        </box>
+      </Box>
+    </Box>
+  );
+};
 export default UnitConversion;
