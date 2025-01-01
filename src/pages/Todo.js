60a38051847914397c2af17c9ab231b0fac5c4bd
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2025-01-01 21:15:01 +0530

    n

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,8 +1,63 @@
-const Todo=()=>{
-    return(
-        <>
-        Todo List
-        </>
-    )
-}
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+
+const Todo = () => {
+  const [name, setName] = useState("");
+  const [student, setStudent] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleAdd = () => {
+   const formatedData = () => {
+      name: name,
+      action:"",
+    };
+    setStudent(formatedData)
+  };
+
+  return (
+    <Box>
+      Todo List
+      <Box>
+        <TextField size="small" onChange={handleName}></TextField>
+        <Button onClick={handleAdd} value={student}>
+          Add
+        </Button>
+      </Box>
+      <Box>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Sr.</TableCell>
+                <TableCell>Name </TableCell>
+                <TableCell>Action </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                {student.localeCompare((data,i)=>(
+
+                    <TableRow>
+                        key=i
+                    </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  );
+};
 export default Todo;
