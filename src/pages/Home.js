8039dcee1f71f135508f6d2ce0ce2d4f638c1d77
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2025-01-04 00:59:17 +0530

    update home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,140 +1,164 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+
+import Login from "./Login";
 
 const Home = () => {
   return (
-    <Box
-      style={{
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-        justifyContent: "center",
-        height: "100%",
-        backgroundColor: "lightblue",
-        width: "100%",
-        borderRadius: "2%",
-        boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
-      }}
-    >
+    <Box  style={{
+      display: "flex",
+      // alignItems: "center",
+      flexDirection: "column",
+      // justifyContent: "center",
+      height: "100%",
+      // backgroundColor: "lightblue",
+      width: "100%",
+      // borderRadius: "2%",
+      // boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+    }}>
+      <Button
+        variant="contained"
+        style={{
+          display: "flex",
+          alignItems: "right",
+          justifyContent: "right",
+        }}
+      >
+        {<Login/>}
+      </Button>
       <Box
         style={{
           display: "flex",
-          //   alignItems: "center",
-          flexDirection: "row",
-          height: "20%",
-          width: "50%",
-          margin: 5,
-          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "100%",
+          backgroundColor: "lightblue",
+          width: "100%",
+          borderRadius: "2%",
+          boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
         }}
       >
-        <Link
-          to="/toDo"
+        <Box
           style={{
-            height: "100%",
-            width: "30%",
             display: "flex",
-            textDecoration: "none",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: "4%",
-            backgroundColor: "#ffff",
-            boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            //   alignItems: "center",
+            flexDirection: "row",
+            height: "20%",
+            width: "50%",
+            margin: 5,
+            justifyContent: "space-around",
           }}
         >
-          <Box>Todo</Box>
-        </Link>
-        <Link
-          to="/convetInSentence"
+          <Link
+            to="/toDo"
+            style={{
+              height: "100%",
+              width: "30%",
+              display: "flex",
+              textDecoration: "none",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "4%",
+              backgroundColor: "#ffff",
+              boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            }}
+          >
+            <Box>Todo</Box>
+          </Link>
+          <Link
+            to="/convetInSentence"
+            style={{
+              height: "100%",
+              width: "30%",
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+              justifyContent: "center",
+              borderRadius: "4%",
+              backgroundColor: "#ffff",
+              boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            }}
+          >
+            <Box>convert in sentence</Box>
+          </Link>
+          <Link
+            to="/factorial"
+            style={{
+              height: "100%",
+              width: "30%",
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+              justifyContent: "center",
+              borderRadius: "4%",
+              backgroundColor: "#ffff",
+              boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            }}
+          >
+            <Box>Factorial</Box>
+          </Link>
+        </Box>
+        <Box
           style={{
-            height: "100%",
-            width: "30%",
             display: "flex",
-            alignItems: "center",
-            textDecoration: "none",
-            justifyContent: "center",
-            borderRadius: "4%",
-            backgroundColor: "#ffff",
-            boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            //   alignItems: "center",
+            flexDirection: "row",
+            height: "20%",
+            width: "50%",
+            margin: 5,
+            justifyContent: "space-around",
           }}
         >
-          <Box>convert in sentence</Box>
-        </Link>
-        <Link
-          to="/factorial"
-          style={{
-            height: "100%",
-            width: "30%",
-            display: "flex",
-            alignItems: "center",
-            textDecoration: "none",
-            justifyContent: "center",
-            borderRadius: "4%",
-            backgroundColor: "#ffff",
-            boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
-          }}
-        >
-          <Box>Factorial</Box>
-        </Link>
-      </Box>
-      <Box
-        style={{
-          display: "flex",
-          //   alignItems: "center",
-          flexDirection: "row",
-          height: "20%",
-          width: "50%",
-          margin: 5,
-          justifyContent: "space-around",
-        }}
-      >
-        <Link
-          to="/ageCalculator"
-          style={{
-            height: "100%",
-            width: "30%",
-            textDecoration: "none",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: "4%",
-            backgroundColor: "#ffff",
-            boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
-          }}
-        >
-          <Box>Age calcultor</Box>
-        </Link>
-        <Link
-          to="/unitConversion"
-          style={{
-            height: "100%",
-            width: "30%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: "4%",
-            backgroundColor: "#ffff",
-            textDecoration: "none",
-            boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
-          }}
-        >
-          <Box>Unit Conversion</Box>
-        </Link>
-        <Link
-          to="/numberToWord"
-          style={{
-            height: "100%",
-            textDecoration: "none",
-            width: "30%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: "4%",
-            backgroundColor: "#ffff",
-            boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
-          }}
-        >
-          <Box>Number to word</Box>
-        </Link>
+          <Link
+            to="/ageCalculator"
+            style={{
+              height: "100%",
+              width: "30%",
+              textDecoration: "none",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "4%",
+              backgroundColor: "#ffff",
+              boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            }}
+          >
+            <Box>Age calcultor</Box>
+          </Link>
+          <Link
+            to="/unitConversion"
+            style={{
+              height: "100%",
+              width: "30%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "4%",
+              backgroundColor: "#ffff",
+              textDecoration: "none",
+              boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            }}
+          >
+            <Box>Unit Conversion</Box>
+          </Link>
+          <Link
+            to="/numberToWord"
+            style={{
+              height: "100%",
+              textDecoration: "none",
+              width: "30%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "4%",
+              backgroundColor: "#ffff",
+              boxShadow: "0px 4px 10px rgba(2, 2, 2, 2)",
+            }}
+          >
+            <Box>Number to word</Box>
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
