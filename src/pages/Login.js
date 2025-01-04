641fce7388490044c8711d22ea019c21fe74aebc
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2025-01-04 22:14:57 +0530

    log in password button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,13 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const SAVED_EMAIL = "rohit.yadav102002@gmail.com";
 const SAVED_PASSWORD = "12345";
@@ -9,6 +17,16 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [confirm, setConfirm] = useState("");
   const [logIn, setLogIn] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const handleShowConfirm = () => {
+    setConfirmPassword((prev) => !prev);
+  };
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -32,9 +50,12 @@ const Login = () => {
           alert("Your email and password not matched");
         }
       } else {
-        alert("Password and confirm password should be same and greater than or equal to 5");
+        alert(
+          "Password and confirm password should be same and greater than or equal to 5"
+        );
       }
     }
+    setLogIn(email || password);
     console.log(email, password, confirm);
   };
 
@@ -72,22 +93,40 @@ const Login = () => {
         <Typography margin={1}>Enter password : </Typography>
         <TextField
           size="small"
-          type="password"
+          type={showPassword ? "text" : "password"}
           label="Enter password"
           value={password}
           onChange={handlePassword}
-          style={{ marginLeft: 25 }}
+          style={{ marginLeft: 25, width: "225px" }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleShowPassword} edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
       </Box>
       <Box style={{ display: "flex", flexDirection: "row", margin: 4 }}>
         <Typography margin={1}>Conferm password : </Typography>
         <TextField
           size="small"
-          type="password"
+          type={confirmPassword ? "text" : "password"}
           label="Confirm password"
           value={confirm}
           onChange={handleConfirmPassword}
-          style={{ marginLeft: 3 }}
+          style={{ marginLeft: 3, width: "225px" }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleShowConfirm} edge="end">
+                  {confirmPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
       </Box>
       <Box
